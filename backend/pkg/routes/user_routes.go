ec056a04ce4c6f077a90c48ef80a7ffa0408554e
chris <mailtochrisnow@gmail.com>
2022-10-17 20:18:19 +0530

    commented seeding and enabled sign up option

--- backend/pkg/routes/user_routes.go
+++ backend/pkg/routes/user_routes.go
@@ -1,19 +1,13 @@
 package routes
 
 import (
-	"github.com/chrispeterjeyaraj/exam-center/backend/pkg/controllers/authentication"
+	controllers "github.com/chrispeterjeyaraj/exam-center/backend/pkg/controllers/authentication"
 
 	"github.com/gorilla/mux"
 )
 
 func UserRoute(router *mux.Router) {
-	// router.HandleFunc("/user", controllers.CreateUser()).Methods("POST")
-	// router.HandleFunc("/user/{userId}", controllers.GetAUser()).Methods("GET")
-	// router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT")
-	// router.HandleFunc("/user/{userId}", controllers.DeleteAUser()).Methods("DELETE")
-	// router.HandleFunc("/users", controllers.GetAllUser()).Methods("GET")
-
-	// router.HandleFunc("/signup", Signup.HandleSignup)
+	router.HandleFunc("/signup", controllers.HandleSignup)
 	router.HandleFunc("/signin", controllers.HandleSignin)
 	// router.HandleFunc("/emailVerify", EmailVerification.HandleEmailVerification)
 	// router.HandleFunc("/forgotPass", ForgotPass.HandleForgotPass)
