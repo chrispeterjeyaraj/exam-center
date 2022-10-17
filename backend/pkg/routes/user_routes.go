4a240c02f46ed1e5a1973e9ad7f0f1a24ebf8084
chris <mailtochrisnow@gmail.com>
2022-10-17 19:55:41 +0530

    added changes for JWT token based authentication and google authentication

--- backend/pkg/routes/user_routes.go
+++ backend/pkg/routes/user_routes.go
@@ -7,9 +7,19 @@ import (
 )
 
 func UserRoute(router *mux.Router) {
-	router.HandleFunc("/user", controllers.CreateUser()).Methods("POST")
-	router.HandleFunc("/user/{userId}", controllers.GetAUser()).Methods("GET")
-	router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT")
-	router.HandleFunc("/user/{userId}", controllers.DeleteAUser()).Methods("DELETE")
-	router.HandleFunc("/users", controllers.GetAllUser()).Methods("GET")
+	// router.HandleFunc("/user", controllers.CreateUser()).Methods("POST")
+	// router.HandleFunc("/user/{userId}", controllers.GetAUser()).Methods("GET")
+	// router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT")
+	// router.HandleFunc("/user/{userId}", controllers.DeleteAUser()).Methods("DELETE")
+	// router.HandleFunc("/users", controllers.GetAllUser()).Methods("GET")
+
+	// router.HandleFunc("/signup", Signup.HandleSignup)
+	router.HandleFunc("/signin", controllers.HandleSignin)
+	// router.HandleFunc("/emailVerify", EmailVerification.HandleEmailVerification)
+	// router.HandleFunc("/forgotPass", ForgotPass.HandleForgotPass)
+	// router.HandleFunc("/codeAuth", ForgotPass.HandleCodeAuth)
+	// router.HandleFunc("/newPass", ForgotPass.HandleNewPassword)
+	router.HandleFunc("/googleAuth", controllers.HandleGoogleAuthenticate)
+	router.HandleFunc("/googleAuthCode", controllers.HandleCodeAuth)
+
 }
